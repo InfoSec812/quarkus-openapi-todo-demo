2d938207f5752d9ecce96f1680a8fda7e3a29d19
Deven Phillips <deven.phillips@redhat.com>
2019-12-02 16:17:01 -0500

    Moved to submodules

---
+++ frontend/src/main/frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="q-app">
+    <my-layout></my-layout>
+  </div>
+</template>
+
+<script>
+import MyLayout from './layouts/MyLayout';
+export default {
+  name: 'App',
+  components: {MyLayout}
+};
+</script>
